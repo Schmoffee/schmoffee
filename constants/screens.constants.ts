23122f575bf5eb09956c2a08cec6658dc16f2d98
meyad-g <79023203+meyad-g@users.noreply.github.com>
2022-09-17 21:27:48 +0100

    feat: add constants folder

---
+++ constants/screens.constants.ts
@@ -0,0 +1,8 @@
+// signup screens
+export const CONST_SCREEN_SIGNUP = 'Signup';
+export const CONST_SCREEN_SIGNUP_ACCEPT_TERMS = 'AcceptTerms';
+export const CONST_SCREEN_SIGNUP_ENTER_MOBILE = 'EnterMobile';
+export const CONST_SCREEN_SIGNUP_VERIFY_MOBILE = 'VerifyMobile';
+export const CONST_SCREEN_SIGNUP_COUNTRY_CODES = 'CountryCodeModal';
+// home
+export const CONST_SCREEN_HOME = 'Home';
