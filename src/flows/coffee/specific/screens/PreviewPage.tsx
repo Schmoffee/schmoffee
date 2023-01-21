5dff3f64edf2148cb0aab49b9188616cb98fd47d
Jad Sbai <jad.sbai@kcl.ac.uk>
2023-01-21 16:08:56 +0000

    Fix basket display

--- src/flows/coffee/specific/screens/PreviewPage.tsx
+++ src/flows/coffee/specific/screens/PreviewPage.tsx
@@ -1,6 +1,6 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useContext, useEffect, useState } from 'react';
-import { ActivityIndicator, Alert, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import React, {useContext, useEffect, useState} from 'react';
+import {ActivityIndicator, Alert, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {
   useStripe,
   initStripe,
@@ -9,7 +9,7 @@ import {
   useApplePay,
   ApplePayButton,
 } from '@stripe/stripe-react-native';
-import { CoffeeRoutes } from '../../../../utils/types/navigation.types';
+import {CoffeeRoutes} from '../../../../utils/types/navigation.types';
 import {
   createGooglePaymentMethod,
   initializeGooglePay,
@@ -25,25 +25,24 @@ import {sendOrder, updatePaymentMethod} from '../../../../utils/queries/datastor
 import {LocalUser, Payment, PaymentParams} from '../../../../utils/types/data.types';
 import Map from '../../../common/components/Map';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
-import { clearStorageSpecificBasket } from '../../../../utils/helpers/storage';
+import {clearStorageSpecificBasket} from '../../../../utils/helpers/storage';
 import ScheduleSection from '../../components/preview/ScheduleSection';
-import { PageLayout } from '../../../common/components/PageLayout';
+import {PageLayout} from '../../../common/components/PageLayout';
 import PreviewSection from '../../components/preview/PreviewSection';
-import { Colors, Spacings } from '../../../common/theme';
-import { useDeepCompareEffect } from 'react-use';
-import { Body, Heading } from '../../../common/typography';
+import {Colors, Spacings} from '../../../common/theme';
+import {useDeepCompareEffect} from 'react-use';
+import {Body, Heading} from '../../../common/typography';
 import LeftChevronBackButton from '../../../common/components/LeftChevronBackButton';
-import { ActionButton } from '../../../common/components/ActionButton';
+import {ActionButton} from '../../../common/components/ActionButton';
 
-
-interface PreviewPageProps { }
+interface PreviewPageProps {}
 export const PreviewPage = (props: PreviewPageProps) => {
-  const { global_state } = useContext(GlobalContext);
-  const { ordering_state } = useContext(OrderingContext);
+  const {global_state} = useContext(GlobalContext);
+  const {ordering_state} = useContext(OrderingContext);
   const navigation = useNavigation<CoffeeRoutes>();
-  const { initPaymentSheet, presentPaymentSheet } = useStripe(); // Stripe hook payment methods
-  const { isGooglePaySupported } = useGooglePay();
-  const { isApplePaySupported } = useApplePay();
+  const {initPaymentSheet, presentPaymentSheet} = useStripe(); // Stripe hook payment methods
+  const {isGooglePaySupported} = useGooglePay();
+  const {isApplePaySupported} = useApplePay();
   const [loading, setLoading] = useState(true);
   const [payment, setPayment] = useState<Payment>('card');
   const [total, setTotal] = useState(0);
@@ -54,7 +53,7 @@ export const PreviewPage = (props: PreviewPageProps) => {
         return acc + item.quantity * (item.price + getOptionsPrice(item));
       }, 0)
       .toFixed(2);
-    setTotal(+totalTemp * 100);
+    setTotal(+totalTemp);
   }, [ordering_state.specific_basket]);
 
   /**
@@ -65,8 +64,8 @@ export const PreviewPage = (props: PreviewPageProps) => {
   function getOptionsPrice(item: OrderItem) {
     return item.options
       ? item.options.reduce(function (acc, option) {
-        return acc + option.price;
-      }, 0)
+          return acc + option.price;
+        }, 0)
       : 0;
   }
 
@@ -124,7 +123,7 @@ export const PreviewPage = (props: PreviewPageProps) => {
   }
 
   async function googlePayCheckout(user_id: string, paymentParams: PaymentParams) {
-    if (!(await isGooglePaySupported({ testEnv: true }))) {
+    if (!(await isGooglePaySupported({testEnv: true}))) {
       Alert.alert('Google Pay is not supported.');
       return;
     }
@@ -175,7 +174,7 @@ export const PreviewPage = (props: PreviewPageProps) => {
 
   const handleCheckout = async () => {
     await checkout(payment);
-    navigation.navigate('TrackOrder', { screen: CONST_SCREEN_ORDER });
+    navigation.navigate('TrackOrder', {screen: CONST_SCREEN_ORDER});
   };
 
   return (
@@ -183,13 +182,10 @@ export const PreviewPage = (props: PreviewPageProps) => {
       <View style={styles.backButton}>
         <LeftChevronBackButton />
       </View>
-      <ScrollView style={styles.previewScrollContainer}
-        contentContainerStyle={{ flexGrow: 1 }}
-      >
-        <View style={{ minHeight: '100%' }}>
-
+      <ScrollView style={styles.previewScrollContainer} contentContainerStyle={{flexGrow: 1}}>
+        <View style={{minHeight: '100%'}}>
           <View style={styles.heading}>
-            <Heading size='default' weight='Bold' color={Colors.white} >
+            <Heading size="default" weight="Bold" color={Colors.white}>
               Summary
             </Heading>
           </View>
@@ -197,7 +193,7 @@ export const PreviewPage = (props: PreviewPageProps) => {
             <BasketSection />
           </View>
           <ScheduleSection />
-          <PreviewSection title="Pick up location" description='23-25 Leather Ln, London EC1N 7TE'>
+          <PreviewSection title="Pick up location" description="23-25 Leather Ln, London EC1N 7TE">
             <View style={styles.mapContainer}>
               <Map cafeIdFilter={ordering_state.current_shop_id} />
             </View>
@@ -206,74 +202,44 @@ export const PreviewPage = (props: PreviewPageProps) => {
           <View style={styles.paymentRoot}>
             <PreviewSection title="Payment method">
               <View style={styles.paymentContainer}>
-                <TouchableOpacity
+                {Platform.OS === 'ios' && (
+                  <ApplePayButton
+                    onPress={handleCheckout}
+                    type="plain"
+                    buttonStyle="black"
+                    borderRadius={4}
+                    style={{
+                      width: '100%',
+                      height: 50,
+                    }}
+                  />
+                )}
+                {Platform.OS !== 'ios' && (
+                  <GooglePayButton
+                    type="standard"
+                    onPress={handleCheckout}
+                    style={{
+                      width: 200,
+                      height: 50,
+                    }}
+                  />
+                )}
 
-                  onPress={() => setPayment('card')}
-                  style={styles.paymentButton}
-                >
-                  <Text style={styles.paymentText}>Card</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-
-                  onPress={() => setPayment('google')}
-                  style={styles.paymentButton}
-                >
-                  <Text style={styles.paymentText}>Google Pay</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-
-                  onPress={() => setPayment('apple')}
-                  style={styles.paymentButton}
-                >
-                  <Text style={styles.paymentText}>Apple Pay</Text>
-                </TouchableOpacity>
+                <ActionButton label="Checkout" onPress={handleCheckout} />
               </View>
             </PreviewSection>
           </View>
           <View style={styles.totalContainer}>
-            <Body size='large' weight='Bold' color={Colors.white} >
+            <Body size="large" weight="Bold" color={Colors.white}>
               Total
             </Body>
-            <Body size='large' weight='Bold' color={Colors.white} >
+            <Body size="large" weight="Bold" color={Colors.white}>
               £{total}
             </Body>
-
           </View>
-
         </View>
-
-        {
-          payment === 'google' && (
-            <GooglePayButton
-              type="standard"
-              onPress={handleCheckout}
-              style={{
-                width: '100%',
-                height: 50,
-              }}
-            />
-          )
-        }
-        {
-          payment === 'apple' && (
-            <ApplePayButton
-              onPress={handleCheckout}
-              type="plain"
-              buttonStyle="black"
-              borderRadius={4}
-              style={{
-                width: '100%',
-                height: 50,
-              }}
-            />
-          )
-        }
-        <ActionButton label="Checkout" onPress={handleCheckout} />
-
       </ScrollView>
-
-    </View >
-
+    </View>
   );
 };
 
@@ -306,7 +272,6 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomLeftRadius: 100,
     borderBottomRightRadius: 100,
-
   },
 
   basketContainer: {
@@ -350,7 +315,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 20,
     marginHorizontal: Spacings.s7,
-
   },
   totalText: {
     color: Colors.white,
--- src/flows/coffee/specific/screens/ShopPage.tsx
+++ src/flows/coffee/specific/screens/ShopPage.tsx
@@ -1,22 +1,12 @@
-import React, {useCallback, useContext, useEffect, useMemo, useRef, useState} from 'react';
+import React, {useContext, useEffect, useMemo, useState} from 'react';
 import {Dimensions, NativeModules, Platform, StyleSheet, TextInput, View} from 'react-native';
 import {OrderingContext} from '../../../../contexts';
 import {Item} from '../../../../models';
-import Animated, {
-  Easing,
-  Extrapolate,
-  interpolate,
-  useAnimatedScrollHandler,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
-import {PreviewPage} from './PreviewPage';
-import {CardSection} from '../../components/menu/CardSection';
-import {Heading} from '../../../common/typography';
-import CustomBackdrop from '../../components/preview/CustomBackdrop';
+import Animated, {Easing, interpolate, useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated';
 import {Colors, Spacings} from '../../../common/theme';
-import CustomHandle from '../../components/preview/CustomHandle';
+import TabNavigator from '../../components/menu/TabNavigator';
+import {BasketPreview} from '../../components/basket/BasketPreview';
+import LeftChevronBackButton from '../../../common/components/LeftChevronBackButton';
 
 const {StatusBarManager} = NativeModules;
 const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 50 : StatusBarManager.HEIGHT;
@@ -27,10 +17,10 @@ export const HEADER_IMAGE_HEIGHT = wHeight / 3;
 
 export const ShopPage = () => {
   const {ordering_state} = useContext(OrderingContext);
-  const translateY = useSharedValue(0);
   const landing_anim = useSharedValue(0);
   const [query, setQuery] = useState('');
   const anim = useSharedValue(0);
+  const basketAnim = useSharedValue(0);
 
   useEffect(() => {
     anim.value = 0;
@@ -38,14 +28,28 @@ export const ShopPage = () => {
       duration: 600,
       easing: Easing.bezier(0.25, 0.1, 0.25, 1),
     });
-  }, []);
+  }, [anim]);
+
+  useEffect(() => {
+    if (ordering_state.specific_basket.length === 0) {
+      // basketAnim.value = 1;
+      basketAnim.value = withTiming(0, {
+        duration: 1000,
+        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+      });
+    }
+    if (ordering_state.specific_basket.length > 0) {
+      basketAnim.value = 1;
+      basketAnim.value = withTiming(1, {
+        duration: 2000,
+        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+      });
+    }
+  }, [basketAnim, ordering_state.specific_basket.length]);
 
   const contains = ({name}: Item, query: string) => {
     const nameLower = name.toLowerCase();
-    if (nameLower.includes(query)) {
-      return true;
-    }
-    return false;
+    return nameLower.includes(query);
   };
   const filtered_items = useMemo(() => {
     const formattedQuery = query.toLowerCase();
@@ -54,22 +58,13 @@ export const ShopPage = () => {
     });
   }, [ordering_state.specific_items, query]);
 
-  const scrollHandler = useAnimatedScrollHandler(event => {
-    translateY.value = event.contentOffset.y;
-  });
   useEffect(() => {
     landing_anim.value = 0;
     landing_anim.value = withTiming(1, {
       duration: 600,
       easing: Easing.bezier(0.25, 0.1, 0.25, 1),
     });
-  }, []);
-
-  // useEffect(() => {
-  //   if (ordering_state.specific_basket.length === 0) {
-  //     bottomSheetRef.current?.snapToPosition(0);
-  //   }
-  // }, [ordering_state]);
+  }, [landing_anim]);
 
   const getCoffees = () => {
     return filtered_items.filter(item => item.type === 'COFFEE');
@@ -80,46 +75,6 @@ export const ShopPage = () => {
   const getPastries = () => {
     return filtered_items.filter(item => item.type === 'SNACKS');
   };
-
-  const rListStyle = useAnimatedStyle(
-    () => ({
-      // opacity: anim.value * 0.5,
-      transform: [
-        {
-          translateY: interpolate(landing_anim.value, [0, 1], [-150, 0]),
-        },
-      ],
-    }),
-    [],
-  );
-
-  const rSearchStyle = useAnimatedStyle(
-    () => ({
-      transform: [
-        {
-          translateY: interpolate(translateY.value, [100, 0], [-100, -20], Extrapolate.CLAMP),
-        },
-      ],
-    }),
-    [],
-  );
-
-  // const bottomSheetRef = useRef<BottomSheet>(null);
-
-  // variables
-  const snapPoints = useMemo(() => ['14%', '90%'], []);
-
-  // callbacks
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log('handleSheetChanges', index);
-  }, []);
-  // const animationConfigs = useBottomSheetSpringConfigs({
-  //   damping: 80,
-  //   overshootClamping: true,
-  //   restDisplacementThreshold: 0.1,
-  //   restSpeedThreshold: 0.1,
-  //   stiffness: 500,
-  // })
 
   const rCircleStyle = useAnimatedStyle(
     () => ({
@@ -133,8 +88,25 @@ export const ShopPage = () => {
     [],
   );
 
+  const rBasketOpenStyle = useAnimatedStyle(
+    () => ({
+      // opacity: anim.value,
+      transform: [
+        {
+          translateY: interpolate(basketAnim.value, [0, 1], [110, 0]),
+        },
+      ],
+    }),
+    [],
+  );
+
   return (
     <View style={styles.root}>
+      {/* back button for navigation */}
+      <View style={styles.button}>
+        <LeftChevronBackButton />
+      </View>
+
       <View style={[styles.itemsContainer]}>
         <View style={styles.header}>
           <Animated.Image
@@ -151,39 +123,12 @@ export const ShopPage = () => {
               placeholder="What do you crave?"
             />
           </Animated.View>
-          <Heading size="default" weight="Extrabld" color={Colors.white}>
-            COFFEE
-          </Heading>
-          {/* <ShopHeader y={translateY} source={require('../../../../assets/pngs/shop.png')} /> */}
+          <TabNavigator tab1={getCoffees()} tab2={getJuices()} tab3={getPastries()} />
         </View>
-        <Animated.ScrollView
-          style={[styles.itemsContainer, rListStyle]}
-          onScroll={scrollHandler}
-          scrollEventThrottle={16}>
-          <View style={styles.container}>
-            <CardSection query={query} items={getCoffees()} />
-            <CardSection items={getJuices()} />
-            <CardSection items={getPastries()} hideDivider />
-          </View>
-        </Animated.ScrollView>
       </View>
-
-      <View style={styles.bottomSheetContainer}>
-        {/*<BottomSheet*/}
-        {/*  index={ordering_state.specific_basket.length > 0 ? 0 : -1}*/}
-        {/*  ref={bottomSheetRef}*/}
-        {/*  snapPoints={snapPoints}*/}
-        {/*  onChange={handleSheetChanges}*/}
-        {/*  onClose={() => {*/}
-        {/*    bottomSheetRef.current?.close();*/}
-        {/*  }}*/}
-        {/*  backdropComponent={props => <CustomBackdrop {...props} />}*/}
-        {/*  handleComponent={props => <CustomHandle {...props} />}>*/}
-        {/*  <View style={styles.previewContainer}>*/}
-        {/*    <PreviewPage />*/}
-        {/*  </View>*/}
-        {/*</BottomSheet>*/}
-      </View>
+      <Animated.View style={[styles.basketContainer, rBasketOpenStyle]}>
+        <BasketPreview />
+      </Animated.View>
     </View>
   );
 };
@@ -204,7 +149,8 @@ const styles = StyleSheet.create({
     paddingTop: 70,
     paddingBottom: 70,
     // justifyContent: 'center',
-    // alignItems: 'center',
+    alignItems: 'center',
+    // backgroundColor: Colors.red,
   },
   itemsContainer: {
     // flex: 1,
@@ -215,6 +161,20 @@ const styles = StyleSheet.create({
     bottom: 0,
     backgroundColor: Colors.greyLight1,
   },
+  basketContainer: {
+    position: 'absolute',
+    bottom: -5,
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+    left: 0,
+    right: 0,
+    borderBottomColor: Colors.greyLight2,
+    backgroundColor: Colors.darkBrown,
+    borderTopRightRadius: 60,
+    borderTopLeftRadius: 60,
+  },
+
   searchInputContainer: {
     backgroundColor: Colors.greyLight1,
     borderRadius: Spacings.s5,
@@ -230,9 +190,16 @@ const styles = StyleSheet.create({
   },
   bottomSheetContainer: {
     flex: 1,
-    zIndex: -10,
-    backgroundColor: Colors.white,
+    zIndex: 1,
+    // height: 300,
+    // backgroundColor: Colors.red,
   },
+  bottomSheet: {
+    backgroundColor: Colors.blue,
+    // zIndex: 1,
+    // height: '100%',
+  },
+
   semiCircle: {
     zIndex: -1,
     position: 'absolute',
@@ -256,5 +223,11 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     marginHorizontal: Spacings.s9,
+  },
+  button: {
+    position: 'absolute',
+    top: 20,
+    left: 0,
+    zIndex: 1,
   },
 });
